3d7e41edb40d8f090d2bd24105b0803be68a7139
Christoffer Karlsson <chka1207@student.miun.se>
2017-02-22 17:10:09 +0100

    ArbetatPass

---
+++ TestMVC/TestMVC/Models/ArbetatPass.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace TestMVC.Models
+{
+    public class ArbetatPass
+    {
+        public Vakt Vakt { get; set; }
+        public Krog Krog { get; set; }
+        public DateTime Datum { get; set; }
+        public DateTime Start { get; set; }
+        public DateTime Slut { get; set; }
+    }
+}
